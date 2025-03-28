4d19e39f5c9825e88ed924dcc4c952f6859256a5
n18011 <n18011@std.it-college.ac.jp>
2025-03-28 15:20:54 +0900

    fix: E2Eテストのポート競合を解決

--- apps/godblessyou/playwright.config.ts
+++ apps/godblessyou/playwright.config.ts
@@ -2,9 +2,9 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'npm run preview',
 		port: 4173,
-		reuseExistingServer: !process.env.CI
+		reuseExistingServer: true
 	},
 	testDir: 'e2e',
 	testMatch: '**/*.test.ts',
